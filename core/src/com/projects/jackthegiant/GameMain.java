d8235f8c5a863f5de4d91c7130178a493d297c1c
Lucas da Silva <lucasf.lsf10051999@gmail.com>
2021-04-02 09:49:11 -0300

    Adding GameInfo

--- core/src/com/projects/jackthegiant/GameMain.java
+++ core/src/com/projects/jackthegiant/GameMain.java
@@ -1,12 +1,14 @@
 package com.projects.jackthegiant;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameMain extends ApplicationAdapter {
+public class GameMain extends Game {
+	private  SpriteBatch batch;
 
 	@Override
 	public void create () {
---
+++ core/src/com/projects/jackthegiant/helpers/GameInfo.java
@@ -0,0 +1,4 @@
+package com.projects.jackthegiant.helpers;
+
+public class GameInfo {
+}
